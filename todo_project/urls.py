d97f7d29640d6e644bab68a43602b55e5378f817
Enngel <engelnieves000@gmail.com>
2026-04-22 14:01:31 +0200

    Add migration files and fix URL error

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.site_header),
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
     path('app/', TemplateView.as_view(template_name='index.html'), name='index'),
